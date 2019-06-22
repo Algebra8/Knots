eff93970ce69b6583752a06731bfc415d4cb1e37
Algebra8 <mmnasrollahi@ucdavis.edu>
2019-06-21 22:18:31 -0700

    Deleted unecessary comments, won't touch master ever again...

--- docs/van.js
+++ docs/van.js
@@ -1,9 +1,3 @@
-// $(document).ready(function() {
-//   $("#sidebarCollapse").on("click", function() {
-//     $("#sidebar").toggleClass("active");
-//   });
-// });
-
 $(document).ready(function() {
   $("#sidebar").mCustomScrollbar({
     theme: "minimal"
